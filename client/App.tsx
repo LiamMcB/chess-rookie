f6439675217e8c75171759ec49fba042fb46f361
liameno16 <liameno16@gmail.com>
2020-12-16 16:40:18 -0800

    added navbar component with preliminary styling

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
+import { Nav } from './components/Nav';
 
 export const App: React.FC = () => {
   return (
-    <div>
-      Hello world
-    </div>
+    <Nav />
   )
 }
---
+++ client/components/Nav.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const Nav: React.FC = () => {
+  return (
+    <nav className='main-nav'>
+      <ul className='nav-text'>
+        <li>
+          <h2 className='page-title'>Chess Rookie</h2>
+        </li>
+      </ul>
+    </nav>
+  )
+}
