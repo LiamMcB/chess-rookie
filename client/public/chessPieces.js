143fac2b2efe72f98f3d5b4dd881217dbd423973
liameno16 <liameno16@gmail.com>
2020-12-17 13:56:53 -0800

    added svg links for all chess pieces

---
+++ client/public/chessPieces.js
@@ -0,0 +1,17 @@
+// File holding links to all pieces
+
+// Black Pieces
+export const BlackKing = 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg';
+export const BlackQueen = 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg';
+export const BlackRook = 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg';
+export const BlackBishop = 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg';
+export const BlackKnight = 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg';
+export const BlackPawn = 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg';
+
+// White Pieces
+export const WhiteKing = 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg';
+export const WhiteQueen = 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg';
+export const WhiteRook = 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg';
+export const WhiteBishop = 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg';
+export const WhiteKnight = 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg';
+export const WhitePawn = 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg';
