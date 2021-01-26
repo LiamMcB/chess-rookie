c3e7769dff45be18fa08bd815537ec114e55daeb
LiamMcB <liameno16@gmail.com>
2021-01-25 16:31:45 -0800

    added inputs for the login form and some basic styling

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 export const Login: React.FC = () => {
   return (
-    <div>
-      Login. 
-      No account? Signup <Link to='/signup'>here</Link>
+    <div className='login-container'>
+      <h1>Login</h1>
+      <input className='form-inputs' placeholder='username'></input>
+      <input className='form-inputs' placeholder='password'></input>
+      <p>
+        No account? Signup <Link to='/signup'>here</Link>
+      </p>
     </div>
   )
 }
