7225f88e950fac53c0f6ebdbc7bb8e76406447a4
LiamMcB <liameno16@gmail.com>
2021-02-01 15:55:53 -0800

    connected login route to backend endpoint and set it up so response updates stop within reducer

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link, Redirect, useHistory } from 'react-router-dom';
 import { BoardContext } from '../BoardContext';
+import { LOGIN_ENDPOINT } from '../constants/endpoints';
 
 interface Props {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
@@ -18,24 +19,50 @@ export const Login: React.FC<Props> = ({ setIsLoading }) => {
   // Function to login as guest
   const loginAsGuest = function() {
     dispatch({ type: 'LOGIN_AS_GUEST' });
-    // Set is loading to true
-    setIsLoading(true);
-    // Push the slash route to history, once user is logged in (1 sec for now), redirecting to board
-    setTimeout(() => {
-      setIsLoading(false);
-      history.push('/');
-    }, 1000);
+    // Redirect to main after 1 second
+    redirectToMain();
   }
   // Function to login the user
   const loginUser = function() {
-    console.log('User:', username, '\n', 'Password:', password);
     // Fetch login data to authenticate user
-    clearFields();
+    fetch(LOGIN_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+    })
+    .then(res => res.json())
+    .then(user => {
+      // Get user information
+      const loggedInUser = {
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname
+      }
+      // Dispatch action to login user
+      dispatch({ type: 'LOGIN_USER', payload: {user: loggedInUser} });
+      // Redirect to main
+      redirectToMain();
+    })
+    .catch(err => console.error('Error in fetch to login endpoint:', err));
   }
   // Function to clear username and password fields
   const clearFields = function() {
     setUsername('');
     setPassword('');
+  }
+  // Function to redirect to main after a second for authenticating and giving time for reducer
+  const redirectToMain = function() {
+    // Set is loading to true
+    setIsLoading(true);
+    // Clear fields in the input form
+    clearFields();
+    // Push the slash route to history, once user is logged in (1 sec for now), redirecting to board
+    setTimeout(() => {
+      setIsLoading(false);
+      history.push('/');
+    }, 1000);
   }
   return (
     <div className='login-container'>
---
+++ client/constants/endpoints.ts
@@ -0,0 +1,4 @@
+// API Endpoints for reference and use in fetching
+export const LOGIN_ENDPOINT: string = 'http://localhost:3000/api/login';
+export const SIGNUP_ENDPOINT: string = 'http://localhost:3000/api/signup';
+export const LOGOUT_ENDPOINT: string = 'http://localhost:3000/api/logout';
