7cb8e24d31350e683848ee9390be60470ab46b35
LiamMcB <liameno16@gmail.com>
2021-02-02 16:04:39 -0800

    added logout functionality along with button, now users can logout after playing

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,6 +1,25 @@
 import * as React from 'react';
+import { BoardContext } from '../BoardContext';
+import { useHistory } from 'react-router-dom';
 
 export const Nav: React.FC = () => {
+  // Get state to see if user is authenticated or not
+  const { state, dispatch } = React.useContext(BoardContext);
+  // Hook into history to redirect to slash on logout
+  const history = useHistory();
+  // Function to logout user and return state to official state
+  const logoutUser = function() {
+    dispatch({type: 'LOGOUT_USER'});
+    redirectToMain();
+  }
+  // Function to redirect to main after a second for logging out and giving time for reducer
+  const redirectToMain = function() {
+    // Push the slash route to history, once user is logged in (1 sec for now), redirecting to board
+    setTimeout(() => {
+      history.push('/');
+    }, 1000);
+  }
+
   return (
     <nav className='main-nav'>
       <ul className='nav-text'>
@@ -8,7 +27,7 @@ export const Nav: React.FC = () => {
           <h2 className='page-title'>Chess Rookie</h2>
         </li>
         <li>
-          <button className='nav-btn'>Logout</button>
+          {state.authenticated && <button className='nav-btn' onClick={logoutUser}>Logout</button>}
         </li>
       </ul>
     </nav>
